bf587a2c93617161fa5d4cb80e0b2a29273edf23
mapineda Pineda <mapineda1222@gmail.com>
2015-12-23 16:21:49 -0600

    got crud functionality to work

--- code/www/js/services.js
+++ code/www/js/services.js
@@ -1,1 +1,17 @@
-angular.module('refrontier.services', []);
+angular.module('refrontier.services', [])
+.factory('User', function() {
+
+  var o = {
+    favorites: []
+  }
+
+   o.addApartmentToFavorites = function(apartment) {
+    // make sure there's a song to add
+    if (!apartment) return false;
+
+    // add to favorites array
+    o.favorites.unshift(apartment);
+  }
+
+  return o;
+});

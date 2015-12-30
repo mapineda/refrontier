b3df73c08fddf9f8884f4299ef0c5616c3f1379a
mapineda Pineda <mapineda1222@gmail.com>
2015-12-29 20:26:04 -0600

    adding loading service

--- code/www/js/controllers.js
+++ code/www/js/controllers.js
@@ -4,30 +4,35 @@ angular.module('refrontier.controllers', ['ionic', 'refrontier.services'])
 /*
 Controller for the discover page
 */
-.controller('DiscoverCtrl', function($scope, $timeout, $ionicLoading, User, Recommendations) {
-
-  var showLoading = function(){
+.controller('DiscoverCtrl', function($scope, $ionicLoading, $timeout, User, Recommendations) {
+  // helper functions for loading
+  var showLoading = function() {
     $ionicLoading.show({
       template: '<i class="ion-loading-c"></i>',
       noBackdrop: true
-    })
-
+    });
   }
 
-  var hideLoading = function(){
+  var hideLoading = function() {
     $ionicLoading.hide();
   }
-// show loading icon when calling on server
+
+  // set loading to true first time while we retrieve songs from server.
   showLoading();
 
   Recommendations.getNextApartments()
     .then(function(){
       $scope.currentApartment = Recommendations.queue[0];
-      Recommendations.playCurrentApartment();
-      
-// hide loading icon after recommendations icons are called
-      hideLoading();
+      return Recommendations.playCurrentApartment();
+
     })
+    .then(function(){
+      // hide loading icon after recommendations icons are called
+      hideLoading();
+      //TODO
+      $scope.currentApartment.loaded = 
+
+    });
 
     // fired when we favorite / skip a song.
  	$scope.sendFeedback = function(bool) {
